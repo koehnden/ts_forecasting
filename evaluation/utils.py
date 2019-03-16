48f698381e7f63fc7a88631aba9b9edccff98f30
koehnden <koehnden@hu-berlin.de>
2019-03-16 22:50:39 +0100

    delete unnecessary python files

--- evaluation/utils.py
+++
@@ -1,8 +0,0 @@
-import scipy.stats as scs
-
-
-def gaussian_confidence_bands(yhat, sigma_hat, alpha):
-    z_value = scs.norm.ppf(alpha)
-    upper = yhat + z_value*sigma_hat
-    lower = yhat - z_value*sigma_hat
-    return lower.clip(0), upper
