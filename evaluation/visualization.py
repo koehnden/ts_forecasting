2a4484b5e7bb9fd068627a5f910b3c2d8c30582d
koehnden <koehnden@hu-berlin.de>
2019-03-17 20:21:13 +0100

    clean up

--- evaluation/visualization.py
+++ evaluation/visualization.py
@@ -10,7 +10,7 @@ def plot_forecast_interval(df_interval, actual, ts_name, figsize=(20, 8)):
     plt.scatter(actual.index, actual.values, label='Actual Values')
     plt.fill_between(df_interval.index, df_interval['lower'], df_interval['upper'], facecolor='blue', alpha=0.2,
                      label='Forecast Interval')
-    plt.plot(df_interval.index, df_interval['yhat'], '--b')
+    plt.plot(df_interval.index, df_interval['yhat'], '--b', label='Predicted Values')
     plt.xticks(fontsize=10)
     plt.yticks(fontsize=10)
     plt.ylabel(ts_name, fontsize=15)
--- models/sarimax.py
+++ models/sarimax.py
@@ -1,28 +1,26 @@
 import itertools
 import warnings
+from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.statespace.sarimax import SARIMAX
-from evaluation.metrics import under_overstock_balance_loss
 
 
-def order_selection(train, test, params, loss_func=under_overstock_balance_loss, **loss_kwargs):
+def order_selection(train, test, params, loss_func=mean_squared_error, **loss_kwargs):
     warnings.filterwarnings("ignore") # to ignore statsmodels warning for unconverged models
     best_score, best_cfg = float("inf"), None
     keys, values = zip(*params.items())
     grid = [dict(zip(keys, v)) for v in itertools.product(*values)]
-    for cfgs in grid:
-        print(cfgs)
-        order = (cfgs['ar_order'], cfgs['difference'], cfgs['ma_order'])
-        seasonal_order = (cfgs['seasonal_ar'], cfgs['seasonal_difference'], cfgs['seasonal_ma'], cfgs['seasonal_periods'])
+    for params in grid:
         try:
-            model_fit = SARIMAX(train, order=order, seasonal_order=seasonal_order).fit()
-            yhat = model_fit.forecast()
-            loss = loss_func(test, yhat, **loss_kwargs)
-            print(loss)
+            model_fit = SARIMAX(train, **params).fit()
         except:
             continue
+        else:
+            yhat = model_fit.forecast(test.shape[0])
+            loss = loss_func(test, yhat, **loss_kwargs)
 
         if loss < best_score:
-            best_score, best_cfg = loss, cfgs
+            best_score, best_params = loss, params
+            print(best_score)
 
-    print('Best ARIMA%s Loss=%.3f' % (best_cfg, best_score))
-    return best_cfg, best_score
+    print('Best ARIMA%s Loss=%.3f' % (best_params, best_score))
+    return best_params, best_score
